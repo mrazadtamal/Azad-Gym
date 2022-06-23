ad6959a7ddf82a4551609187191e6e4a234d8b03
mrazadtamal <mrazadt@gmail.com>
2022-06-24 00:48:05 +0600

    nav  done

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ import ExerciseDetail from "./pages/ExerciseDetail";
 function App() {
   return (
     <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
-      <Navbar />
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/exercise/:id" element={<ExerciseDetail />} />
+          {/* <Route path="/exercise/:id" element={<ExerciseDetail />} /> */}
         </Routes>
+        <Footer />
       </BrowserRouter>
-      <Footer />
     </Box>
   );
 }
