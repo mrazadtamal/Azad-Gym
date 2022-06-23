9c3c39f946de29de83f86ec01f14b15588183b70
mrazadtamal <mrazadt@gmail.com>
2022-06-24 01:09:36 +0600

    hero

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -3,9 +3,15 @@ import { Box, Stack, Typography } from "@mui/material";
 
 const HeroBanner = () => {
   return (
-    <div>
-      <h1>HeroBanner</h1>
-    </div>
+    <Box
+      sx={{ mt: { lg: "212px", xs: "70px" }, ml: { sm: "50px" } }}
+      position="relative"
+      p="20px"
+    >
+      <Typography color="#0E2F5A" fontWeight="400" fontSize="26px">
+        Azad GYM
+      </Typography>
+    </Box>
   );
 };
 
