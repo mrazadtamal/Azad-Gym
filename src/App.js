3040af22d6f895f750bdb151424ff2f2fb1f9f0c
mrazadtamal <mrazadt@gmail.com>
2022-06-23 23:27:23 +0600

    setup  done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
 import { Box } from "@mui/material";
 
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>azad app </h1>
-    </div>
+    <Box width="400px">
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </Box>
   );
 }
 
