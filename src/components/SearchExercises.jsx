c54288e3a1d9077b487966206b6170d6c23aeb9a
mrazadtamal <mrazadt@gmail.com>
2022-06-26 00:30:08 +0600

    Search Exercises

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -4,7 +4,18 @@ import { Box, Stack, Typography, Button, TextField } from "@mui/material";
 const SearchExercises = () => {
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
-      <Typography>azad</Typography>
+      <Typography
+        fontWeight="700"
+        sx={{ fontSize: { lg: "44px", xs: "50px" } }}
+        mb="50px"
+        mt="60px"
+        textAlign="center"
+      >
+        search
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField />
+      </Box>
     </Stack>
   );
 };
